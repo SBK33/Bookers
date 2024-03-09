a33c908cae079dfe2a24b46f44ad9767c03fa826
SBK33 <157976694+SBK33@users.noreply.github.com>
2024-03-09 15:50:35 +0000

    [Add] destroy成功時のフラッシュメッセージ追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -36,6 +36,7 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
+    flash[:notice] = "Book was successfully destroyed."
   end
 
 end
